21cb9213daae8dc21d4569a76fd032d408d3e1be
Odeth Ayellén Peña Gómez <odethayellen@hotmail.com>
2025-03-07 09:54:04 -0600

    Agregar archivos de proyecto.

---
+++ Serialize/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Serialize;
+using System.Text.Json;
+
+public class Program
+{
+    public static void Main()
+    {
+        var weatherForecast = new WeatherForecast
+        {
+            Date = DateTime.Parse("2019-08-01"),
+            TemperatureCelsius = 25,
+            Summary = "Hot"
+        };
+
+        string jsonString = JsonSerializer.Serialize(weatherForecast);
+
+        Console.WriteLine(jsonString);
+    }
+}
+
