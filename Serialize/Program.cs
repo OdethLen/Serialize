74573a75e7ce875f27d777a8ac74869a61db594e
Odeth Ayellén Peña Gómez <odethayellen@hotmail.com>
2025-03-07 10:00:17 -0600

    ...

--- Serialize/Program.cs
+++ Serialize/Program.cs
@@ -2,20 +2,29 @@
 using Serialize;
 using System.Text.Json;
 
-public class Program
+var weatherForecast = new WeatherForecast
 {
-    public static void Main()
-    {
-        var weatherForecast = new WeatherForecast
-        {
-            Date = DateTime.Parse("2019-08-01"),
-            TemperatureCelsius = 25,
-            Summary = "Hot"
-        };
+Date = DateTime.Parse("2019-08-01"),
+TemperatureCelsius = 25,
+Summary = "Hot"
+};
 
-        string jsonString = JsonSerializer.Serialize(weatherForecast);
+string jsonString = JsonSerializer.Serialize(weatherForecast);
+Console.WriteLine(jsonString);
 
-        Console.WriteLine(jsonString);
-    }
-}
+var weatherForecastT = new WeatherForecast
+{
+    Date = DateTime.Parse("2019-08-01"),
+    TemperatureCelsius = 25,
+    Summary = "Hot"
+};
+
+string fileName = "WeatherForecast.json";
+string jsonStringg = JsonSerializer.Serialize(weatherForecast);
+File.WriteAllText(fileName, jsonString);
+
+Console.WriteLine(File.ReadAllText(fileName));
+
+
+
 
